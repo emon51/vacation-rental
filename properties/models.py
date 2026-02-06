ef23f7f65735746ddee17625c00db8de5aab6db6
emon51 <emanur11235@gmail.com>
2026-02-06 12:30:46 +0600

    Create Location, Property, and Image models

--- properties/models.py
+++ properties/models.py
@@ -1,3 +1,36 @@
 from django.db import models
 
-# Create your models here.
+
+class Location(models.Model):
+    name = models.CharField(max_length=200)
+    country = models.CharField(max_length=100)
+    state = models.CharField(max_length=100, blank=True)
+    city = models.CharField(max_length=100)
+    
+    def __str__(self):
+        return f"{self.city}, {self.country}"
+
+
+class Property(models.Model):
+    location = models.ForeignKey(Location, on_delete=models.CASCADE, related_name='properties')
+    title = models.CharField(max_length=300)
+    description = models.TextField()
+    price_per_night = models.DecimalField(max_digits=10, decimal_places=2)
+    bedrooms = models.IntegerField()
+    bathrooms = models.IntegerField()
+    max_guests = models.IntegerField()
+    
+    class Meta:
+        verbose_name_plural = 'Properties'
+    
+    def __str__(self):
+        return self.title
+
+
+class Image(models.Model):
+    property = models.ForeignKey(Property, on_delete=models.CASCADE, related_name='images')
+    image_url = models.URLField(max_length=500)
+    is_primary = models.BooleanField(default=False)
+    
+    def __str__(self):
+        return f"Image for {self.property.title}"
