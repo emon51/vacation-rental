77c77b5c33397b81061b70bae192abf517910f4b
emon51 <emanur11235@gmail.com>
2026-02-06 16:30:00 +0600

    Register models in admin with filters and inline images

--- properties/admin.py
+++ properties/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
+from .models import Location, Property, Image
 
-# Register your models here.
+
+class ImageInline(admin.TabularInline):
+    model = Image
+    extra = 1
+
+
+@admin.register(Location)
+class LocationAdmin(admin.ModelAdmin):
+    list_display = ['city', 'state', 'country']
+    list_filter = ['country', 'state']
+    search_fields = ['city', 'state', 'country', 'name']
+
+
+@admin.register(Property)
+class PropertyAdmin(admin.ModelAdmin):
+    list_display = ['title', 'location', 'price_per_night', 'bedrooms', 'bathrooms', 'max_guests']
+    list_filter = ['location__country', 'location__city', 'bedrooms', 'bathrooms']
+    search_fields = ['title', 'description', 'location__city']
+    inlines = [ImageInline]
+
+
+@admin.register(Image)
+class ImageAdmin(admin.ModelAdmin):
+    list_display = ['property', 'image_url', 'is_primary']
+    list_filter = ['is_primary']
