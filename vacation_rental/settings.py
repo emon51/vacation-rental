0095cc36f20e32990c7e0e8fb5fbc717d2fcea03
emon51 <emanur11235@gmail.com>
2026-02-10 12:19:15 +0600

    Configure media files settings

--- vacation_rental/settings.py
+++ vacation_rental/settings.py
@@ -106,3 +106,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+
+# Media files
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+
+# Pagination
+PROPERTIES_PER_PAGE = 6
